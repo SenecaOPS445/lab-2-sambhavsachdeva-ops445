2456eb1e7425e5905c1344aa5e07b1628ae75e98
sambhav <ssachdeva25@myseneca.ca>
2025-01-23 21:01:16 -0500

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Sambhav Sachdeva
+# Author ID: ssachdeva25
+# Date Created: 2025/01/23
+
+
+timer = 10
+
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+
+print("blast off!")
